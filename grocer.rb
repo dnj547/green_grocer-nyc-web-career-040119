73e47a7e01b460c4d8391cf7db08efbca41f0c04
Danielle Jasper <dnj547@live.com>
2019-03-17 14:43:02 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -36,8 +36,8 @@ end
 def apply_clearance(cart)
   cart.each do |item|
     if item[1][:clearance]
-      price = item[1][:price]
-      item[1][:price] = price*0.8.round(1)
+      price = item[1][:price] * 0.8
+      item[1][:price] = price.round(1)
     end
   end
 end
