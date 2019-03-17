4f80a6e663a8a56e1d12f3c5a55b97d5748db615
Danielle Jasper <dnj547@live.com>
2019-03-17 13:18:13 +0000

    Automatically backed up by Learn

--- grocer.rb
+++ grocer.rb
@@ -1,9 +1,44 @@
+require 'pry'
 def consolidate_cart(cart)
-  # code here
+  new_hash = {}
+  cart.each do |item|
+    item.each do |item_name, item_attributes|
+      if !new_hash.key?(item_name)
+        new_hash[item_name] = item_attributes
+        new_hash[item_name][:count] = 1
+      elsif new_hash.key?(item_name)
+        new_hash[item_name][:count] += 1
+      end
+    end
+  end
+  new_hash
 end
 
 def apply_coupons(cart, coupons)
-  # code here
+  new_hash = cart
+  coupons.each do |coupon|
+    coupon.each do |coupon_key, coupon_value|
+      cart.each do |item_name, item_attributes|
+        item_attributes.each do |attributes_key, attributes_value|
+          if coupon[:item] == item_name
+            if coupon[:num] >= item_attributes[:count]
+              without = item_attributes[:count] - coupon[:num]
+              new_hash["#{item_name} W/COUPON"] = item_attributes
+              new_hash["#{item_name} W/COUPON"][:count] = coupon[:num]
+              new_hash["#{item_name} W/COUPON"][:price] = coupon[:cost]
+              if without == 0
+                new_hash.delete(item_name)
+              else
+                new_hash[item_name][:count] = without
+              end
+            end
+          end
+        end
+      end
+    end
+  end
+  new_hash
+  binding.pry
 end
 
 def apply_clearance(cart)
